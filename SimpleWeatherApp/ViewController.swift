b7839ebc12ddf2f962e6f2b60d6758f561f0a37f
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-07-09 14:06:42 -0400

    Initial Commit

---
+++ SimpleWeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleWeatherApp
+//
+//  Created by Kaleb Jubar on 2024-07-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
