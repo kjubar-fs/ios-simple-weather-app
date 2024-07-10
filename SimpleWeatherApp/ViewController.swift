8f39d4680514c3e9d91ffea7a3ade8f72ef03c32
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-07-10 13:00:57 -0400

    Add more info for location display, stubs for temperature units

--- SimpleWeatherApp/ViewController.swift
+++ SimpleWeatherApp/ViewController.swift
@@ -10,17 +10,26 @@ import CoreLocation
 
 class ViewController: UIViewController {
 
+    // weather display Views
     @IBOutlet weak var curWeatherLabel: UILabel!
     @IBOutlet weak var curWeatherImageView: UIImageView!
     @IBOutlet weak var curTempStackView: UIStackView!
     @IBOutlet weak var curTempLabel: UILabel!
-    @IBOutlet weak var curCityLabel: UILabel!
+    @IBOutlet weak var curLocLabel: UILabel!
     
+    // temperature unit buttons
     @IBOutlet weak var tempCButton: UIButton!
     @IBOutlet weak var tempFButton: UIButton!
     
+    // location services-related vars
     var locationMgr: CLLocationManager?
     var hasLocationAuth: Bool = false
+    
+    // current weather/location info
+    var weatherDispName: String?
+    var weatherIcon: UIImage?
+    var weatherTemp: Double?
+    var locationStr: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,12 +43,14 @@ class ViewController: UIViewController {
         locationMgr?.delegate = self
     }
 
+    /// Triggered when the C unit button is pressed, switches display to Celsius.
     @IBAction func tempCBtnPressed() {
-        
+        setTempUnit(inCelsius: true)
     }
     
+    /// Triggered when the F unit button is pressed, switches display to Fahrenheit.
     @IBAction func tempFBtnPressed() {
-        
+        setTempUnit(inCelsius: false)
     }
     
     /// Function for when location button is pressed.
@@ -74,19 +85,31 @@ class ViewController: UIViewController {
         curWeatherLabel.isHidden = !show
         curWeatherImageView.isHidden = !show
         curTempStackView.isHidden = !show
-        curCityLabel.isHidden = !show
+        curLocLabel.isHidden = !show
     }
     
-    /// Update the data in the weather info displays.
-    func setWeatherInfo(weatherCode: Int, temp: Double, city: String) {
+    /// Set the data in the class and refresh weather info displays.
+    func setWeatherInfo(weatherCode: Int, temp: Double, location: String) {
         // get the display info for the weather code
         let (name, icon) = lookupWeatherCode(weatherCode)
         
+        // store in variables
+        weatherDispName = name
+        weatherIcon = icon
+        weatherTemp = temp
+        locationStr = location
+        
+        // update display
+        updateWeatherDisplay()
+    }
+    
+    /// Update the display views for the weather info with the info in the class.
+    func updateWeatherDisplay() {
         // update the display
-        curWeatherLabel.text = name
-        curWeatherImageView.image = icon
-        curTempLabel.text = String(temp)
-        curCityLabel.text = city
+        curWeatherLabel.text = weatherDispName
+        curWeatherImageView.image = weatherIcon
+        curTempLabel.text = String(weatherTemp ?? 0)
+        curLocLabel.text = locationStr
     }
     
     /// Given a weather code from the API, return a tuple with the display-friendly name and an icon.
@@ -95,9 +118,21 @@ class ViewController: UIViewController {
         
         return (name: "Sunny", icon: UIImage(systemName: "sun.max.fill")?.withRenderingMode(.alwaysOriginal))
     }
+    
+    /// Set the temperature unit.
+    /// If inCelsius is true, set to Celsius. Otherwise, set to Fahrenheit.
+    func setTempUnit(inCelsius: Bool) {
+        if (inCelsius) {
+            
+        } else {
+            
+        }
+    }
 }
 
 extension ViewController: CLLocationManagerDelegate {
+    /// Delegate function for location authorization changes.
+    /// Enables location updates when successfully authorized, disables when not authorized.
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
         // check what auth response we got
         switch manager.authorizationStatus {
@@ -117,6 +152,8 @@ extension ViewController: CLLocationManagerDelegate {
         }
     }
     
+    /// Delegate function for location changes.
+    /// Get information and weather for the new location and display it.
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         // get the most recent location or return
         guard let location = locations.last else {
@@ -125,16 +162,16 @@ extension ViewController: CLLocationManagerDelegate {
         
         // use a Geocoder to be able to get city
         // this is asynchronous and has a callback for when the operation is complete
-        CLGeocoder().reverseGeocodeLocation(location) { [weak self] result, err in
+        CLGeocoder().reverseGeocodeLocation(location) { [weak self] results, err in
             // bind a strong self reference in this closure, since it's an async callback
             // like how we did with Firebase auth
-            guard let strongSelf = self else {
+            guard let strongSelf = self, let resultLoc = results?.first else {
                 return
             }
             
             // show weather info
             strongSelf.setShowWeatherInfo(true)
-            strongSelf.setWeatherInfo(weatherCode: 1, temp: 18.0, city: result?.first?.locality ?? "")
+            strongSelf.setWeatherInfo(weatherCode: 1, temp: 18.0, location: "\(resultLoc.locality ?? ""), \(resultLoc.administrativeArea ?? ""), \(resultLoc.country ?? "")")
         }
     }
 }
