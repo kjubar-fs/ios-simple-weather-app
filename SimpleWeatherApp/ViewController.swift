d71b52dcc71bf7db225d1d10e7978c23475ec10e
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-07-10 10:37:18 -0400

    Add outlets and actions

--- SimpleWeatherApp/ViewController.swift
+++ SimpleWeatherApp/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var curWeatherLabel: UILabel!
+    @IBOutlet weak var curWeatherImageView: UIImageView!
+    @IBOutlet weak var curTempStackView: UIStackView!
+    @IBOutlet weak var curTempLabel: UILabel!
+    
+    @IBOutlet weak var tempCButton: UIButton!
+    @IBOutlet weak var tempFButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tempCBtnPressed() {
+        
+    }
+    
+    @IBAction func tempFBtnPressed() {
+        
+    }
 }
 
