869100ba0d0a416959754660ef8e14cce7e4c3ec
S. Berk Ozdemir <sberk99@hotmail.com>
2024-07-11 16:29:25 -0400

    Seperate file created for Weather UI

--- SimpleWeatherApp/ViewController.swift
+++ SimpleWeatherApp/ViewController.swift
@@ -120,46 +120,7 @@ class ViewController: UIViewController {
         stopUpdatingLocation()
         getWeatherForLocation(location)
     }
-    
-    // MARK: - weather UI functions
-    
-    /// Show or hide the weather info displays.
-    func setShowWeatherInfo(_ show: Bool) {
-        curWeatherLabel.isHidden = !show
-        curWeatherImageView.isHidden = !show
-        curTempStackView.isHidden = !show
-        curLocLabel.isHidden = !show
-    }
-    
-    /// Set the data in the class and refresh weather info displays.
-    func setWeatherInfo(weatherCode: Int, weatherString: String, tempC: Double, tempF: Double, location: String) {
-        // store in variables
-        weatherDispName = weatherString
-        weatherIcon = lookupWeatherCode(weatherCode)
-        weatherTempC = tempC
-        weatherTempF = tempF
-        locationStr = location
-        
-        // update display
-        updateWeatherDisplay()
-    }
-    
-    /// Given a weather code from the API, return a tuple with the display-friendly name and an icon.
-    func lookupWeatherCode(_ code: Int) -> UIImage? {
-        // TODO: implement
-        
-        return UIImage(systemName: "sun.max.fill")?.withRenderingMode(.alwaysOriginal)
-    }
-    
-    /// Update the display views for the weather info with the info in the class.
-    func updateWeatherDisplay() {
-        // update the display
-        curWeatherLabel.text = weatherDispName
-        curWeatherImageView.image = weatherIcon
-        curTempLabel.text = displayTempInC ? String(weatherTempC ?? 0) : String(weatherTempF ?? 0)
-        curLocLabel.text = locationStr
-    }
-    
+
     // MARK: - temperature unit functions
     
     /// Triggered when the C unit button is pressed, switches display to Celsius.
---
+++ SimpleWeatherApp/WeatherUI.swift
@@ -0,0 +1,49 @@
+//
+//  WeatherUI.swift
+//  SimpleWeatherApp
+//
+//  Created by Berk Ozdemir on 2024-07-11.
+//
+
+import UIKit
+
+extension ViewController {
+    
+    /// Show or hide the weather info displays.
+    func setShowWeatherInfo(_ show: Bool) {
+        curWeatherLabel.isHidden = !show
+        curWeatherImageView.isHidden = !show
+        curTempStackView.isHidden = !show
+        curLocLabel.isHidden = !show
+    }
+    
+    /// Set the data in the class and refresh weather info displays.
+    func setWeatherInfo(weatherCode: Int, weatherString: String, tempC: Double, tempF: Double, location: String) {
+        // store in variables
+        weatherDispName = weatherString
+        weatherIcon = lookupWeatherCode(weatherCode)
+        weatherTempC = tempC
+        weatherTempF = tempF
+        locationStr = location
+        
+        // update display
+        updateWeatherDisplay()
+    }
+    
+    /// Given a weather code from the API, return a tuple with the display-friendly name and an icon.
+    func lookupWeatherCode(_ code: Int) -> UIImage? {
+        // TODO: implement
+        
+        return UIImage(systemName: "sun.max.fill")?.withRenderingMode(.alwaysOriginal)
+    }
+    
+    /// Update the display views for the weather info with the info in the class.
+    func updateWeatherDisplay() {
+        // update the display
+        curWeatherLabel.text = weatherDispName
+        curWeatherImageView.image = weatherIcon
+        curTempLabel.text = displayTempInC ? String(weatherTempC ?? 0) : String(weatherTempF ?? 0)
+        curLocLabel.text = locationStr
+    }
+    
+}
